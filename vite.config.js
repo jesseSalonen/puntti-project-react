cc29d601f99e0bd0519428f482dbb0fa678f963d
jSalonen <60747806+jesseSalonen@users.noreply.github.com>
2023-04-13 14:29:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    ssr: false
+  },
   server: {
     watch: {
       usePolling: true,
@@ -11,5 +15,15 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
+    middleware: {
+      // add a rewrite rule to handle requests for routes in your application
+      // that should be handled by your React application
+      1: (req, res, next) => {
+        if (req.originalUrl.startsWith('/*')) {
+          req.url = req.originalUrl.replace('/*', '/');
+        }
+        next();
+      }
+    }
   },
 });
