4c77a3aeb5fc29ae5ccd4947a5e1d8d5df0aa36d
salonje1 <jesse.salonen@wakaru.fi>
2023-04-21 14:54:33 +0300

    i18next

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
-import "./i18n";
+import "./translations/i18n.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/translations/i18n.js
+++ src/translations/i18n.js
@@ -1,6 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import
+
+const resources = {
+    en: {
+        // Namespaces
+        dashboard:
+    }
+}
 
 i18n
   // detect user language
