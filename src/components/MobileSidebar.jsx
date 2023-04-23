be6e06745028f1fb8f909ae1ca3bcf5354267182
salonje1 <jesse.salonen@wakaru.fi>
2023-04-23 16:45:36 +0300

    Sidebar component

---
+++ src/components/MobileSidebar.jsx
@@ -0,0 +1,22 @@
+function MobileSidebar() {
+  return (
+    <div 
+      className="
+        fixed
+        top-0
+        left-0
+        w-3/4
+        h-full
+        z-1
+        flex
+        flex-col
+        items-left
+        justify-center
+        bg-grey-500
+      "
+    >
+    </div>
+  )
+}
+
+export default MobileSidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import ExerciseItem from "../components/ExerciseItem";
 import Spinner from "../components/Spinner";
 import { getExercises, reset } from "../features/exercises/exerciseSlice";
 import { useTranslation } from "react-i18next";
+import MobileSidebar from "../components/MobileSidebar";
 
 function Dashboard() {
   const { t } = useTranslation("dashboard");
@@ -36,6 +37,8 @@ function Dashboard() {
   if (isLoading) {
     return <Spinner />;
   }
+
+  <MobileSidebar />
 
   return (
     <>
