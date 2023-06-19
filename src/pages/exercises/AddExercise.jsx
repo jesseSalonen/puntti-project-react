a6988bda07fe399277e310a090c06924c85a39ea
jesseSalonen <jesse.salonen@tuni.fi>
2023-06-19 19:03:55 +0300

    Sivupalkin painike valkoiseksi

--- src/pages/exercises/AddExercise.jsx
+++ src/pages/exercises/AddExercise.jsx
@@ -8,9 +8,7 @@ function AddExercise() {
   return (
     <>
       <div className="mb-12 py-0 px-5 font-bold">
-        <h1 className="mb-4 flex items-center justify-center text-5xl max-sm:text-4xl">
-          {t("dashboard")}
-        </h1>
+        <h1 className="mb-4 text-5xl max-sm:text-4xl">{t("dashboard")}</h1>
         <p className="text-4xl text-gray-400 max-sm:text-2xl">
           {t("addExercisesFromBelow")}
         </p>
