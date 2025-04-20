8864f03ba98ac5a0fca5dbf9dc3b4f8801e2a602
jesseSalonen <jesse3778@gmail.com>
2025-04-20 10:08:56 +0300

    treenien luonnin ulkoasuparannuksia

--- src/pages/workouts/WorkoutSearch.jsx
+++ src/pages/workouts/WorkoutSearch.jsx
@@ -11,35 +11,12 @@ import {
   selectWorkouts,
 } from "../../features/workouts/workoutSlice";
 import Spinner from "../../components/common/Spinner";
+import {FaPlus} from 'react-icons/fa';
 
 function WorkoutSearch() {
   const { t } = useTranslation("workouts");
   const dispatch = useDispatch();
   const { workouts, isLoading, isError, message } = useSelector(selectWorkouts);
-
-  const linkButton = (buttonText) => {
-    return (
-      <button
-        className="
-          flex 
-          cursor-pointer
-          items-center 
-          justify-center 
-          rounded-md 
-          bg-[#222C34]
-          py-3 
-          px-5 
-          text-center 
-          text-white
-          hover:bg-[#3A4549]
-          hover:text-green-700
-          max-md:hidden
-        "
-      >
-        {t(buttonText)}
-      </button>
-    );
-  };
 
   useEffect(() => {
     dispatch(getWorkouts());
@@ -62,28 +39,38 @@ function WorkoutSearch() {
   return (
     <>
       <div className="mx-auto w-3/4 max-sm:w-11/12">
+        <div className="mb-6 flex justify-end">
+          <Link to="/workouts/add">
+            <button className="
+              flex
+              cursor-pointer
+              items-center
+              justify-center
+              rounded-md
+              bg-gradient-to-r
+              from-green-500
+              to-green-100
+              py-3
+              px-5
+              max-md:py-2
+              max-md:px-3
+              text-green-800
+              max-w-xs
+              whitespace-nowrap
+            ">
+              <FaPlus className="mr-2" />
+              {t("addWorkout")}
+            </button>
+          </Link>
+        </div>
         {workouts.length > 0 ? (
-          <>
-            <div className="grid grid-cols-2 gap-3 max-sm:grid-cols-1">
-              {workouts.map((workout) => (
-                <WorkoutItem key={workout._id} workout={workout} />
-              ))}
-            </div>
-            <ul>
-              <li>
-                <Link to="/workouts/add">{linkButton("addWorkouts")}</Link>
-              </li>
-            </ul>
-          </>
+          <div className="grid grid-cols-2 gap-3 max-sm:grid-cols-1">
+            {workouts.map((workout) => (
+              <WorkoutItem key={workout._id} workout={workout} />
+            ))}
+          </div>
         ) : (
-          <>
-            <h3>{t("noWorkouts")}</h3>
-            <ul>
-              <li>
-                <Link to="/workouts/add">{linkButton("addWorkouts")}</Link>
-              </li>
-            </ul>
-          </>
+          <h3>{t("noWorkouts")}</h3>
         )}
       </div>
     </>
